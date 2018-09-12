2c637f82ae1c6a7d3002c4c172091f2851e1403d
xuzg <xzg.nju@gmail.com>
2018-09-13 00:38:47 +0800

    modify view and url

--- polls/views.py
+++ polls/views.py
@@ -2,36 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from .models import Choice
+from django.urls import reverse
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
-
-
-# Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    print(question)
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+from django.views import generic
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
+        print(request.POST)
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        print(selected_choice)
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
@@ -47,6 +30,19 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+    
